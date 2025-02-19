a1b72d37240cc6238f81419413efda473a16f560
Hams Kuveluka <kuvelukahams@gmail.com>
2025-02-19 11:36:59 +0100

    Updated about/leaderboard-map page content

--- src/app/(marketing)/about/leaderboard-map/page.tsx
+++ src/app/(marketing)/about/leaderboard-map/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <div className="min-h-[50vh] bg-white">
-      <h1>Historique</h1>
+      <div className="h-[15rem] bg-[#112673] flex flex-col justify-end p-10">
+        <h1 className="text-white text-4xl font-bold">Organigrame</h1>
+      </div>
     </div>
   );
 }
