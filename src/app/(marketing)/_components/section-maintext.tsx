70081dba6d73ebbaa685a7491ac912f2a6ac8ad0
Hams Kuveluka <kuvelukahams@gmail.com>
2025-02-26 14:56:31 +0100

    Updated section-maintext layout

--- src/app/(marketing)/_components/section-maintext.tsx
+++ src/app/(marketing)/_components/section-maintext.tsx
@@ -1,3 +1,5 @@
+import { PolySansWeb } from "@/constants/fonts";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -17,8 +19,16 @@ export default function SectionMainText() {
         </div>
 
         <div className="w-1 h-[13rem] bg-slate-100"></div>
-        <div className="flex-1">
-          <h1 className="text-black text-2xl md:text-3xl font-bold">
+        <div className="flex-1 space-y-2">
+          <h3 className="text-black text-sm md:text-base font-bold">
+            Notre principale mission
+          </h3>
+          <h1
+            className={cn(
+              "text-black text-2xl md:text-3xl font-bold",
+              PolySansWeb.className
+            )}
+          >
             Mise en œuvre opérationnelle de la politique budgétaire en termes
             d’exécution des dépenses et de suivi des recettes perçues par les
             administrations financières
