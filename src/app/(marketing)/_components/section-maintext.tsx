74fa672100e98fb83400d04f357de5059ffa0d11
Hams Kuveluka <kuvelukahams@gmail.com>
2025-02-26 14:52:31 +0100

    Updated section-maintext

--- src/app/(marketing)/_components/section-maintext.tsx
+++ src/app/(marketing)/_components/section-maintext.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function SectionMainText() {
   return (
     <section className="bg-white">
-      <div className="container mx-auto flex justify-center p-10 py-24 gap-10">
+      <div className="container mx-auto flex justify-center items-center p-10 py-24 gap-10">
         <div className="flex-1 flex justify-end">
           <div>
             <Image
@@ -16,9 +16,9 @@ export default function SectionMainText() {
           </div>
         </div>
 
-        <div className="w-1 h-[15rem] bg-slate-100"></div>
+        <div className="w-1 h-[13rem] bg-slate-100"></div>
         <div className="flex-1">
-          <h1 className="text-black text-2xl md:text-4xl font-bold">
+          <h1 className="text-black text-2xl md:text-3xl font-bold">
             Mise en œuvre opérationnelle de la politique budgétaire en termes
             d’exécution des dépenses et de suivi des recettes perçues par les
             administrations financières
