687ce44b741e2f700717e3b714cc4fcf6c8b61f4
Hams Kuveluka <kuvelukahams@gmail.com>
2025-03-06 15:57:01 +0100

    Updated not found logo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import HeaderSection from "./(marketing)/_components/section-header";
 import { FooterSection } from "@/components/ui/footer-section";
-import { Camera, Card } from "iconsax-react";
+import { Camera, Card, CloudCross } from "iconsax-react";
 
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col">
       <HeaderSection />
       <div className="flex-1 flex flex-col justify-center items-center bg-slate-50 space-y-4">
-        <Camera className="h-5 w-5" />
+        <CloudCross className="size-32" />
         <h2 className="text-2xl font-bold">Contenu introuvable</h2>
         <p>La ressource demandée n'a pas été trouvée</p>
         <Link href="/">Retourner sur la page d'accueil</Link>
